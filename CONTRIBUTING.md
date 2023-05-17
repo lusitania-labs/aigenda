ec3c4fa8120327b53e389f8bdcdbfe98399e921e
alfredo-mfaria <alfredo.faria88@gmail.com>
2023-05-17 22:25:47 +0200

    Initial commit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,36 @@
+# Contributing to Aigenda
+
+First off, thank you for considering contributing to Aigenda. It's people like you that make Aigenda such a great tool.
+
+## Where do I go from here?
+
+If you've noticed a bug or have a feature request, make one! 
+It's generally best if you get confirmation of your bug or approval for your feature request this way before starting to code.
+
+If you have a general question, creating an issue is fine.
+
+## Fork & create a branch
+
+If this is something you think you can fix, then fork Aigenda and create a branch with a descriptive name.
+
+A good branch name would be (where issue #325 is the ticket you're working on):
+
+```sh
+git checkout -b 325-add-xyz-plugin
+```
+
+## Did you find a bug?
+Ensure the bug was not already reported by searching on GitHub under Issues.
+
+If you're unable to find an open issue addressing the problem, open a new one. Be sure to include a title and clear description, as much relevant information as possible, and a code sample or an executable test case demonstrating the expected behavior that is not occurring.
+
+## Implement your fix or feature
+At this point, you're ready to make your changes! Feel free to ask for help; everyone is a beginner at first.
+
+## Merging a PR (maintainers only)
+
+1. Branch was created from `main`.
+2. Code that should be tested.
+3. If you've changed APIs, documentation was updated.
+4. Ensure the test suite passes.
+5. Make sure your code lints.
---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.0.6"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+}
+
+group = "com.lusitania.labs"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2022.0.2"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
